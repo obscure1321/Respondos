be55194d2cdb7c2092d38f40f7f982bd7a4bff05
obscure1321 <obscure1321@gmail.com>
2024-02-03 12:00:33 +0300

    added coin image and animated the flips

--- Respondos/UI/Views/HeadsOrTailsView.swift
+++ Respondos/UI/Views/HeadsOrTailsView.swift
@@ -10,6 +10,16 @@ import UIKit
 final class HeadsOrTailsView: UIView {
     private var headsOrTailsButton = GeneralButton()
     
+    private var coinImageView: UIImageView = {
+        let element = UIImageView()
+        element.contentMode = .scaleToFill
+        element.clipsToBounds = true
+        element.backgroundColor = .clear
+        element.image = UIImage(named: "heads")
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .systemBackground
@@ -19,6 +29,8 @@ final class HeadsOrTailsView: UIView {
                   title: "T O S S",
                   bottom: 80,
                   side: 120)
+        setConstraints()
+        
         headsOrTailsButton.addTarget(self, action: #selector(tossButton), for: .touchUpInside)
     }
     
@@ -30,9 +42,38 @@ final class HeadsOrTailsView: UIView {
 private extension HeadsOrTailsView {
     func addViews() {
         addSubview(headsOrTailsButton)
+        addSubview(coinImageView)
     }
     
+    func setConstraints() {
+        coinImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(80)
+            $0.trailing.equalToSuperview().offset(-80)
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(coinImageView.snp.width)
+        }
+    }
+    
+    func animateFlip(_ view: UIImageView) {
+        let flip = CATransition()
+        flip.startProgress = 0.0
+        flip.endProgress = 1.0
+        flip.type = CATransitionType(rawValue: "flip")
+        flip.subtype = CATransitionSubtype(rawValue: "fromRight")
+        flip.duration = 0.18
+        flip.repeatCount = 5
+        view.layer.add(flip, forKey: "transition")
+    }
+        
     @objc func tossButton() {
-        print("heads or tails")
+        animateFlip(coinImageView)
+        print("sdasd")
+        let status = Bool.random()
+        
+        if status {
+            coinImageView.image = UIImage(named: "heads")
+        } else {
+            coinImageView.image = UIImage(named: "tails")
+        }
     }
 }
--- Respondos/UI/Views/YesOrNoView.swift
+++ Respondos/UI/Views/YesOrNoView.swift
@@ -9,8 +9,10 @@ import UIKit
 import SnapKit
 
 final class YesOrNoView: UIView {
+    // MARK: - properties
     private var yesOrNoButton = GeneralButton()
     
+    // MARK: - initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .systemBackground
@@ -20,6 +22,7 @@ final class YesOrNoView: UIView {
                   title: "A N S W E R",
                   bottom: 80,
                   side: 120)
+        
         yesOrNoButton.addTarget(self, action: #selector(answerButton), for: .touchUpInside)
     }
     
@@ -28,6 +31,7 @@ final class YesOrNoView: UIView {
     }
 }
 
+// MARK: - extension for flow funcs
 private extension YesOrNoView {
     func addViews() {
         addSubview(yesOrNoButton)
